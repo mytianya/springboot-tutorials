90ff5874542ad464cc06d18174e49f81766205ce
dsys <dsyslove@163.com>
2020-08-01 00:10:28 +0800

    springboot2.x快速开始

---
+++ springboot-tutorials/src/main/java/vip/codehome/springboot/tutorials/SpringbootTutorialsApplication.java
@@ -0,0 +1,13 @@
+package vip.codehome.springboot.tutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTutorialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTutorialsApplication.class, args);
+    }
+
+}
