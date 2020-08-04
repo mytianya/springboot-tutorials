6da11adb918aaf42a2d9dba4af49622318ce5f62
dsys <dsyslove@163.com>
2020-08-04 09:27:56 +0800

    xx

--- springboot-tutorials/src/main/java/vip/codehome/springboot/tutorials/controller/SwaggerUserController.java
+++ springboot-tutorials/src/main/java/vip/codehome/springboot/tutorials/controller/SwaggerUserController.java
@@ -6,6 +6,9 @@ import vip.codehome.springboot.tutorials.common.R;
 import vip.codehome.springboot.tutorials.dto.LoginDTO;
 import vip.codehome.springboot.tutorials.vo.UserInfoVO;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
 @Api(tags = "Swagger注解测试类")
 public class SwaggerUserController {
@@ -29,5 +32,26 @@ public class SwaggerUserController {
         userInfoVO.setNickname("编程之家");
         userInfoVO.setToken("xxx");
         return R.ok(userInfoVO);
+    }
+    @GetMapping("/date")
+    public R<Date> testDate(Date date){
+        System.out.println(date);
+        return R.ok(date);
+    }
+    @GetMapping("/date1")
+    public R<Date> testDate(String date1){
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy/MM/dd");
+        try {
+            Date date = sdf1.parse(date1);//拿到Date对象
+            String str = sdf2.format(date);//输出格式：2017-01-22 09:28:33
+            System.out.println(str);
+            return R.ok(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return R.failed("");
+        }
+
+
     }
 }
